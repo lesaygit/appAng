b8ae64bb91ee5ebdd0ce917c81731ed96e16e02f
Lazaro Jorge <lazarojorge412@gmail.com>
2022-07-31 00:23:05 +0100

    Implementacion de error, y paginador a la tabla de productos

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,22 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { tap } from 'rxjs';
 import { Producto } from 'src/app/interfaces/productos.interface';
 import { ProductosService } from 'src/app/services/productos.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.scss']
 })
-export class ProductosComponent implements OnInit {
+export class ProductosComponent implements AfterViewInit {
 
-  productos!: Producto [];
-  displayedColumns = ['id', 'name', 'description', 'price', 'stock'];
+  displayedColumns: string[] = ['id', 'name', 'price', 'stock', 'operaciones'];
+  dataSource = new MatTableDataSource<Producto>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   constructor(public productosService: ProductosService) { }
 
   ngOnInit(): void {
-    this.productosService.getProductos().subscribe(prod => {
-      this.productos = prod;
-    });
+    this.productosService.getProductos().pipe(
+      tap(
+        data => {
+          this.dataSource.data = data;
+        }
+      ) 
+    ).subscribe(
+      {error:err => alert(err)}
+    )
   }
 }
